bd332c86c1d912ea77b91907254d9ee4d91ac350
Dmitry Kozlov <thaidem19@gmail.com>
2019-08-07 15:21:29 +0300

    Задание 3 / Сценарий логина

---
+++ test_login.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_get_login(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
+    driver.find_element_by_name('login').click()
