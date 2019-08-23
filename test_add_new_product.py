c3a10bbcfb26777838b84b7f564a914708567ba4
Dmitry Kozlov <thaidem19@gmail.com>
2019-08-23 16:03:11 +0300

    Задание 12 / сценарий добавления товара

--- test_add_new_product.py
+++ test_add_new_product.py
@@ -1,6 +1,10 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 
 
 @pytest.fixture
@@ -15,7 +19,7 @@ def driver(request):
     return wd
 
 
-def test_check_countries(driver):
+def test_add_new_product(driver):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name('username').send_keys('admin')
     driver.find_element_by_name('password').send_keys('admin')
@@ -23,4 +27,44 @@ def test_check_countries(driver):
 #
     driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog")
     driver.find_element(By.LINK_TEXT, 'Add New Product').click()
-
+    driver.find_element_by_css_selector('input[type="radio"][value="1"]').click()
+    driver.find_element_by_css_selector('[name="name[en]"]').send_keys("Big Duck")
+    driver.find_element_by_css_selector('[name="code"]').send_keys("1111111")
+    driver.find_element_by_css_selector('input[name="categories[]"][value="0"]').click()
+    driver.find_element_by_css_selector('input[name="categories[]"][value="1"]').click()
+    driver.find_element_by_css_selector('input[name="product_groups[]"][value="1-3"]').click()
+    driver.find_element_by_css_selector('input[name="quantity"]').clear()
+    driver.find_element_by_css_selector('input[name="quantity"]').send_keys("11")
+    select = Select(driver.find_element_by_css_selector('select[name="sold_out_status_id"]'))
+    select.select_by_visible_text('-- Select --')
+    driver.find_element_by_css_selector('input[name="new_images[]"]').send_keys(os.getcwd() + "/bigduck.jpg")
+    driver.find_element_by_css_selector('input[name="date_valid_from"]')\
+        .send_keys(Keys.HOME + datetime.today().strftime("%d%m%Y"))
+#
+    driver.find_element(By.LINK_TEXT, 'Information').click()
+    select = Select(driver.find_element_by_css_selector('select[name="manufacturer_id"]'))
+    select.select_by_value("1")
+    driver.find_element_by_css_selector('input[name="keywords"]').send_keys("big duck")
+    driver.find_element_by_css_selector('input[name="short_description[en]"]').send_keys("It's very big duck")
+    driver.find_element_by_css_selector('div.trumbowyg-editor')\
+        .send_keys("This unbelievable big duck should like your!")
+    driver.find_element_by_css_selector('input[name="head_title[en]"]').send_keys("Very big duck for sale")
+    driver.find_element_by_css_selector('input[name="meta_description[en]"]').send_keys("This big duck for your")
+#
+    driver.find_element(By.LINK_TEXT, 'Prices').click()
+    driver.find_element_by_css_selector('input[name="purchase_price"]').clear()
+    driver.find_element_by_css_selector('input[name="purchase_price"]').send_keys("1000")
+    select = Select(driver.find_element_by_css_selector('select[name="purchase_price_currency_code"]'))
+    select.select_by_value("USD")
+    driver.find_element_by_css_selector('input[name="gross_prices[USD]"]').clear()
+    driver.find_element_by_css_selector('input[name="gross_prices[USD]').send_keys("1200")
+    driver.find_element_by_css_selector('input[name="gross_prices[EUR]"]').clear()
+    driver.find_element_by_css_selector('input[name="gross_prices[EUR]').send_keys("1000")
+    driver.find_element_by_css_selector('button[name="save"]').click()
+#
+    driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
+    table_product = driver.find_element_by_css_selector('table.dataTable')
+    rows_product = table_product.find_elements_by_css_selector('tr.row')
+    for item in rows_product:
+        if item.find_element(By.XPATH, './td[3]').text == "Big Duck":
+            print("Новый товар найден в каталоге!")
