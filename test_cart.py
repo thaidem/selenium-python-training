f28ba21fa2c3f90ab0f60583ffa32adbce032d49
Dmitry Kozlov <thaidem19@gmail.com>
2019-08-28 12:32:19 +0300

    Задание 13 / Корзина

--- test_cart.py
+++ test_cart.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 
 @pytest.fixture
@@ -14,35 +14,26 @@ def driver(request):
     wd = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe")
     # wd = webdriver.Firefox()
     # wd = webdriver.Ie()
-    # wd.implicitly_wait(10)
-    # wait = WebDriverWait(driver, 10)
     request.addfinalizer(wd.quit)
     return wd
 
 
 def test_cart(driver):
     wait = WebDriverWait(driver, 10)
-    driver.get("https://litecart.stqa.ru/en/")
-    # driver.get("http://localhost/litecart/en/")
-    driver.find_element_by_css_selector('.product').click()
-    quantity = driver.find_element_by_css_selector('span.quantity')
-    print(quantity.text)
-    driver.find_element_by_css_selector('button[name="add_cart_product"]').click()
-    wait.until(EC.staleness_of(quantity))
-    quantity = driver.find_element_by_css_selector('span.quantity')
-    print(quantity.text)
-    # driver.get("https://litecart.stqa.ru/en/")
-    # driver.find_element_by_css_selector('.product').click()
-    # driver.find_element_by_css_selector('button[name="add_cart_product"]').click()
-    # quantity = driver.find_element_by_css_selector('span.quantity')
-    # print(quantity.text)
-    # driver.refresh()
-    # wait.until(EC.staleness_of(quantity))
-    # driver.get("https://litecart.stqa.ru/en/")
-    # driver.find_element_by_css_selector('.product').click()
-    # driver.find_element_by_css_selector('button[name="add_cart_product"]').click()
-    # quantity = driver.find_element_by_css_selector('span.quantity')
-    # print(quantity.text)
-    # driver.refresh()
-    # wait.until(EC.staleness_of(quantity))
-    # pass
+#
+    for s in range(3):
+        driver.get("https://litecart.stqa.ru/en/")
+        driver.find_element_by_css_selector('.product').click()
+        if len(driver.find_elements_by_css_selector('[name="options[Size]"]')) != 0:
+            Select(driver.find_element_by_css_selector('[name="options[Size]"]')).select_by_value('Large')
+        quantity = driver.find_element_by_css_selector('span.quantity')
+        new_quantity = int(quantity.text) + 1
+        driver.find_element_by_css_selector('button[name="add_cart_product"]').click()
+        wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, "quantity"), str(new_quantity)))
+    driver.find_element_by_css_selector('#cart .link').click()
+#
+    driver.find_element_by_css_selector('.act').click()
+    while len(driver.find_elements_by_css_selector('[value="Remove"]')) != 0:
+        driver.find_element_by_css_selector('[value="Remove"]').click()
+        item = driver.find_element_by_css_selector('.dataTable td.item')
+        wait.until(EC.staleness_of(item))
