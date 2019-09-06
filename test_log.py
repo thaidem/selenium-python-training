81e880c56de89f0d1136a5ac8e9dda1cc796a3c7
Dmitry Kozlov <thaidem19@gmail.com>
2019-09-06 12:07:05 +0300

    Update test_log.py

--- test_log.py
+++ test_log.py
@@ -4,14 +4,15 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver(request):
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    wd = webdriver.Chrome(chrome_options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("start-maximized")
+    # wd = webdriver.Chrome(chrome_options=options)
+    wd = webdriver.Chrome(desired_capabilities={"proxy": {"proxyType": "MANUAL", "httpProxy": "localhost:8888"}})
     request.addfinalizer(wd.quit)
     return wd
 
 
-def test_check_countries(driver):
+def test_logs(driver):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name('username').send_keys('admin')
     driver.find_element_by_name('password').send_keys('admin')
@@ -28,4 +29,3 @@ def test_check_countries(driver):
         for entry in driver.get_log('browser'):
             print(entry)
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
-
